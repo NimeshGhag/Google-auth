2ade38a8dadca1a14832b351bd9d02d53fe261b0
NimeshGhag <ghagnimesh490@gmail.com>
2026-03-26 12:16:14 +0530

    feat: integrate Passport.js for authentication

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 const path = require("path");
 require("dotenv").config({ path: path.resolve(__dirname, "../.env") });
 const express = require("express");
+const passport = require("passport");
 
 
 const app = express();
 
+app.use(passport.initialize());
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
